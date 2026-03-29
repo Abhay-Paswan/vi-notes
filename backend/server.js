60ee666ebc55bb18cc2c54870b9d7cf0a308baec
abhay-paswan <gk8100304@gmail.com>
2026-03-29 12:51:24 +0530

    fix:routes

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,12 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use('/api/auth', authRoutes);
+
 app.use('/', (req, res) => {
   res.send('Server is running');
 })
-
-// Routes
-app.use('/api/auth', authRoutes);
 
 // Database Connection
 mongoose.connect(process.env.MONGODB_URI)
