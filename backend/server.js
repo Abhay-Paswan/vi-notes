5855afae21126142d90ee649fb52831b177b3ef6
abhay-paswan <gk8100304@gmail.com>
2026-03-29 12:42:02 +0530

    feat: add home route

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use('/', (req, res) => {
+  res.send('Server is running');
+})
+
 // Routes
 app.use('/api/auth', authRoutes);
 
